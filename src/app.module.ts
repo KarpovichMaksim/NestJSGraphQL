cfcc31490ee8435656a51ff304d15620ac888e61
Maksim <the.butterfly.incorporated@gmail.com>
2021-12-10 23:49:05 +0300

    Create cats module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
+import { CatsModule } from './cats/cats.module';
 
 @Module({
   imports: [
@@ -9,6 +10,7 @@ import { GraphQLModule } from '@nestjs/graphql';
       debug: false,
       playground: false,
     }),
+    CatsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
