97583a8cb11c1a088031b9eeea44fa46b0171c42
Maksim <the.butterfly.incorporated@gmail.com>
2021-12-11 23:48:57 +0300

    Add implimentation authors service

--- src/authors/authors.service.ts
+++ src/authors/authors.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthorsService {}
+export class AuthorsService {
+  findOneById(id: number) {
+    throw new Error('Method not implemented.');
+  }
+}
