d305b19d6a19ed26496009808f16b92d56486b23
Maksim <the.butterfly.incorporated@gmail.com>
2021-12-11 23:49:07 +0300

    Add implimentation posts service

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  findAll(arg0: { authorId: number }) {
+    throw new Error('Method not implemented.');
+  }
+}
