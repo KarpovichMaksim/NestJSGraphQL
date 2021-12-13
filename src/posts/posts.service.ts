677c35dc4db7665bc35664a2040c7d3c7e2fd3af
Maksim <the.butterfly.incorporated@gmail.com>
2021-12-13 23:49:09 +0300

    Add post service implementation

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { Post } from './models/post.model';
 
 @Injectable()
 export class PostsService {
-  findAll(arg0: { authorId: number }) {
-    throw new Error('Method not implemented.');
+  async create(data: Post): Promise<Post> {
+    return {} as any;
+  }
+
+  async findOneById(id: number): Promise<Post> {
+    return {} as any;
+  }
+
+  async findAll(arg0: { authorId: number }): Promise<Post[]> {
+    return [] as Post[];
+  }
+
+  async remove(id: number): Promise<boolean> {
+    return true;
   }
 }
