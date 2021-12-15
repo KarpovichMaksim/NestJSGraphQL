8b75cdda7bf3ec921d57f18bbe3512d895b2e336
Maksim <the.butterfly.incorporated@gmail.com>
2021-12-15 23:38:21 +0300

    Change author resolver

--- src/authors/authors.resolver.ts
+++ src/authors/authors.resolver.ts
@@ -11,21 +11,20 @@ import { PostsService } from 'src/posts/posts.service';
 import { AuthorsService } from './authors.service';
 import { Author } from './models/author.model';
 
-@Resolver((of) => Author)
+@Resolver('Author')
 export class AuthorsResolver {
   constructor(
     private authorsService: AuthorsService,
     private postsService: PostsService,
   ) {}
 
-  @Query((returns) => Author, { name: 'author' })
-  async getAuthor(@Args('id', { type: () => Int }) id: number) {
+  @Query()
+  async author(@Args('id') id: number) {
     return this.authorsService.findOneById(id);
   }
 
-  @Resolver('Author')
   @ResolveField()
-  async getPosts(@Parent() author) {
+  async posts(@Parent() author) {
     const { id } = author;
     return this.postsService.findAll({ authorId: id });
   }
